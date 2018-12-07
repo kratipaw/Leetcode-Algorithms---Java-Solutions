ab3c0ab46439b046f7d2ae9f9a5169b34c9c917d
Krati Paw <kratipaw3@gmail.com>
2018-12-07 09:42:03 +0530

    PRoblem 36 : Valid Sudoku

---
+++ problems/_36_ValidSudoku.java
@@ -0,0 +1,136 @@
+package problems;
+
+import java.util.HashSet;
+
+/**
+    Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according 
+    to the following rules:
+
+	-	Each row must contain the digits 1-9 without repetition.
+	-	Each column must contain the digits 1-9 without repetition.
+	-	Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+	
+	The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+	Example 1:
+	
+	Input:
+	[
+	  ["5","3",".",".","7",".",".",".","."],
+	  ["6",".",".","1","9","5",".",".","."],
+	  [".","9","8",".",".",".",".","6","."],
+	  ["8",".",".",".","6",".",".",".","3"],
+	  ["4",".",".","8",".","3",".",".","1"],
+	  ["7",".",".",".","2",".",".",".","6"],
+	  [".","6",".",".",".",".","2","8","."],
+	  [".",".",".","4","1","9",".",".","5"],
+	  [".",".",".",".","8",".",".","7","9"]
+	]
+	Output: true
+	Example 2:
+	
+	Input:
+	[
+	  ["8","3",".",".","7",".",".",".","."],
+	  ["6",".",".","1","9","5",".",".","."],
+	  [".","9","8",".",".",".",".","6","."],
+	  ["8",".",".",".","6",".",".",".","3"],
+	  ["4",".",".","8",".","3",".",".","1"],
+	  ["7",".",".",".","2",".",".",".","6"],
+	  [".","6",".",".",".",".","2","8","."],
+	  [".",".",".","4","1","9",".",".","5"],
+	  [".",".",".",".","8",".",".","7","9"]
+	]
+	Output: false
+	Explanation: Same as Example 1, except with the 5 in the top left corner being 
+	    modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+	
+	Note:
+	
+	- A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+	- Only the filled cells need to be validated according to the mentioned rules.
+	- The given board contain only digits 1-9 and the character '.'.
+	- The given board size is always 9x9.
+
+	
+ */
+public class _36_ValidSudoku {
+
+	public static void main(String[] args) {
+
+		char[][] board = {
+		            	  {'5','3','.','.','7','.','.','.','.'},
+		            	  {'6','.','.','1','9','5','.','.','.'},
+		            	  {'.','9','8','.','.','.','.','6','.'},
+		            	  {'8','.','.','.','6','.','.','.','3'},
+		            	  {'4','.','.','8','.','3','.','.','1'},
+		            	  {'7','.','.','.','2','.','.','.','6'},
+		            	  {'.','6','.','.','.','.','2','8','.'},
+		            	  {'.','.','.','4','1','9','.','.','5'},
+		            	  {'.','.','.','.','8','.','.','7','9'}
+						};
+		
+		System.out.println(new _36_ValidSudoku().isValidSudoku(board));
+	}
+
+	public boolean isValidSudoku(char[][] board) {
+        
+        for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				
+				if(!isSafe(board, i, j))
+					return false;
+			}
+		}
+    
+    	return true;
+        
+    }
+    
+    public boolean isSafe(char[][] board, int row, int col){
+        
+        HashSet<Character> set = new HashSet<Character>();
+        
+        //row clash
+        for(int i = 0 ; i < 9 ; i++) {
+            
+            if(set.contains(board[row][i]))
+                return false;
+            
+            if(board[row][i] != '.')
+                set.add(board[row][i]);
+        }
+        
+        set.clear();
+        
+        //col clash
+        for(int i = 0 ; i < 9 ; i++) {
+            
+            if(set.contains(board[i][col]))
+                return false;
+            
+            if(board[i][col] != '.')
+                set.add(board[i][col]);
+        }
+
+        set.clear();
+        
+        //box clash
+        
+        int boxRow = row - (row % 3);
+        int boxCol = col - (col % 3);
+
+        for (int i = boxRow; i < boxRow + 3; i++)  { 
+            for (int j = boxCol; j < boxCol + 3; j++) { 
+                
+                if(set.contains(board[i][j]))
+                return false;
+                
+                if(board[i][j] != '.')  
+                    set.add(board[i][j]); 
+            }
+        }
+        
+        return true;
+    }
+}
